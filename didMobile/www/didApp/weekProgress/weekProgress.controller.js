ef20ad575957448edb1ed63e3efe3ddfe9e8df40
Ishanka Lakshan Wasalage <isankalakshan@gmail.com>
2015-09-15 16:12:04 +0530

    transferred features to this app from old one

---
+++ didMobile/www/didApp/weekProgress/weekProgress.controller.js
@@ -0,0 +1,164 @@
+angular.module('didApp.weekProgressController', ['angularMoment'])
+
+.controller('weekProgressCtrl',['$scope','$stateParams','didAppDataService',weekProgressCtrl])
+
+function weekProgressCtrl($scope,$stateParams,didAppDataService){
+
+  $scope.weekCount = $stateParams.currentWeekNumber*1;
+  $scope.yearCount = $stateParams.currentYearNumber*1;
+  console.log($scope.weekCount);
+  ionic.Platform.ready(function(){
+    $scope.timesheet = [];
+    didAppDataService.loadData();
+    $scope.weekStartend = getWeekStartEnd($scope.weekCount,$scope.yearCount);
+    $scope.weeklyTimesheet = [];
+    getAllWeekEntries($scope.weekCount,$scope.yearCount);
+    setWeekTimeSheet($scope.weekCount,$scope.yearCount);
+    console.log($scope.weekCount);
+
+  });
+
+  var allWeekTimeEntries = [];
+
+  $scope.$on('didApp.didmobiledata',function(_,result){
+    result.feed.entry.forEach(function(b){
+      $scope.timesheet.push({
+          id:b.content.properties.Id.__text,
+          title:b.content.properties.Title.__text,
+          startTime:b.content.properties.PzlStartTime.__text,
+          endTime:b.content.properties.PzlEndTime.__text,
+          duration:b.content.properties.PzlDurationHours.__text,
+
+          timezone:b.content.properties.PzlTimeZone.__text,
+          description:b.content.properties.PzlDescription.__text,
+          state:b.content.properties.PzlState.__text,
+          category:b.content.properties.PzlCategory.__text,
+          location:b.content.properties.PzlLocation.__text,
+
+          organizer:b.content.properties.PzlOrganizer.__text,
+          customerKeyId:b.content.properties.PzlCustomerKeyId.__text,
+          resourceKeyId:b.content.properties.PzlResourceKeyId.__text,
+          dateImported:b.content.properties.PzlDateImported.__text,
+          lastUpdatedOn:b.content.properties.PzlLastUpdatedOn.__text,
+
+          sourceSystemId:b.content.properties.PzlSourceSystemId.__text,
+          weekNumber:b.content.properties.PzlWeekNumber.__text,
+          yearNumber:b.content.properties.PzlYearNumber.__text,
+          modified:b.content.properties.Modified.__text,
+          created:b.content.properties.Created.__text
+      });
+    });
+  });
+
+
+  function getWeekStartEnd(weekNumber,yearNumber){
+      var weekStartEnd = {
+        weekNumber : weekNumber,
+        weekStart : moment(String(weekNumber)+ yearNumber,'WWYYYY').startOf('isoWeek').format('MMM, dddd DD'),
+        weekEnd : moment(String(weekNumber)+yearNumber,'WWYYYY').endOf('isoWeek').day(-2).format('MMM, dddd DD')
+      };
+    return weekStartEnd;
+  };//end of getWeekStartEnd()
+
+
+  function getAllWeekEntries(weekNumber,yearNumber){
+    allWeekTimeEntries = [];
+    $scope.weekStartend = getWeekStartEnd(weekNumber,yearNumber);
+    $scope.timesheet.forEach(function(entry){
+      if (entry.weekNumber==weekNumber && entry.yearNumber==yearNumber) {
+            allWeekTimeEntries.push(entry);
+      }//end if
+    });//end forEach
+    console.log(allWeekTimeEntries);
+    return allWeekTimeEntries;
+  };
+
+  function getTotalHoursPerDay(date){
+    var totalHours = 0;
+    allWeekTimeEntries.forEach(function(day){
+      if (moment(day.startTime).format('MMM, dddd DD') == date) {
+        totalHours +=day.duration*1
+      }
+    });//end foreach
+    return totalHours;
+  };//end of getTotalHoursPerDay
+
+  function getStateOfDay(date){
+    var suggestCount= 0;
+    allWeekTimeEntries.forEach(function(day){
+      if (moment(day.startTime).format('MMM, dddd DD') == date && day.state =='Suggested') {
+        suggestCount +=1
+      }
+    });//end foreach
+    if (suggestCount>0) {
+      return false;
+    }else {
+      return true;
+    }
+  };//end of getStateOfDay
+
+  function getStateOfWeek(){
+    var suggestedCount =0;
+    var approvedCount = 0;
+
+    allWeekTimeEntries.forEach(function(day){
+      if (day.state =='Suggested') {
+        suggestedCount +=1
+      }
+      if (day.state =='Approved') {
+        approvedCount +=1
+      }
+    });//end foreach
+    if (approvedCount > 0) {
+      console.log('Display already confirmed');
+      return 'Confirmed';
+    }else if (suggestedCount >0 ) {
+      console.log('dont display button');
+      return 'Suggested';
+    }else if (suggestedCount==0 && approvedCount ==0) {
+      console.log('Display this is what i DId button');
+      return 'DidIt';
+
+    }
+  };
+
+  function setWeekTimeSheet(weekNumber,yearNumber){
+    for (var i = 1; i < 6; i++) {
+      var weekStartDate = moment(String(weekNumber)+ yearNumber,'WWYYYY').startOf('isoWeek').day(i).format('MMM, dddd DD')
+          $scope.weeklyTimesheet.push({
+            date : moment(weekStartDate,'MMM, dddd DD').format('ddd'),
+            hours : getTotalHoursPerDay(weekStartDate),
+            state : getStateOfDay(weekStartDate)
+          });
+    };//end for
+
+  };
+
+  $scope.addOneWeek =function(){
+    $scope.weekCount += 1;
+    console.log($scope.weekCount);
+    $scope.weeklyTimesheet = [];
+    $scope.weekStartend = getWeekStartEnd($scope.weekCount,$scope.yearCount);
+    getAllWeekEntries($scope.weekCount,$scope.yearCount);
+    setWeekTimeSheet($scope.weekCount,$scope.yearCount);
+    $scope.stateWeek =getStateOfWeek();
+  };//end of addOneWeek()
+
+  $scope.substractOneWeek =function(){
+    $scope.weekCount -= 1;
+    console.log($scope.weekCount);
+    $scope.weeklyTimesheet = [];
+    $scope.weekStartend = getWeekStartEnd($scope.weekCount,$scope.yearCount);
+    getAllWeekEntries($scope.weekCount,$scope.yearCount);
+    setWeekTimeSheet($scope.weekCount,$scope.yearCount);
+    $scope.stateWeek =getStateOfWeek();
+  };//end of substractOneWeek()
+
+  $scope.onLoad = function(){
+
+    getAllWeekEntries('37','2015');
+    setWeekTimeSheet('37','2015');
+  };
+
+
+};//end of weekProgressCtrl
--- didMobile/www/js/app.js
+++ didMobile/www/js/app.js
@@ -1,23 +1,20 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+var didApp = angular.module('didApp', ['ionic',
+                           'angularMoment',
+                           'didApp.weekProgressController',
+                           'didApp.loginController',
+                           'didApp.dayProgressController',
+                           'didApp.projectProgressController',
+                           'didApp.service'
+                          ])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
   });
@@ -25,61 +22,53 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
+  .state('login',{
+        url:'/login',
+        templateUrl:'didApp/login/login.html',
+        controller:'loginCtrl'
+      })
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: 'didApp/tabs.html'
   })
-
   // Each tab has its own nav history stack:
+  .state('tab.weekProgress',{
+        url:'/weekProgress',
+        views: {
+          'tab-home': {
+            templateUrl: 'didApp/weekProgress/weekProgress.html',
+            controller: 'weekProgressCtrl',
+          }
+        },
+        params : {currentWeekNumber : '',
+              currentYearNumber : ''}
+      })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
+  .state('tab.dayProgress', {
+      url: '/dayProgress',
       views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        'tab-home': {
+          templateUrl: 'didApp/dayProgress/dayProgress.html',
+          controller: 'dayProgressCtrl'
         }
       }
     })
 
-  .state('tab.account', {
-    url: '/account',
+  .state('tab.projectProgress', {
+    url: '/projectProgress',
     views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+      'tab-home': {
+        templateUrl: 'didApp/projectProgress/projectProgress.html',
+        controller: 'projectProgressCtrl'
       }
     }
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/login');
 
 });
