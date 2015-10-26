ff4271d54b0ab107b16cf1ecb96ddcaffab4d632
Ishanka Lakshan Wasalage <isankalakshan@gmail.com>
2015-10-26 15:09:49 +0530

    removed unnecessary files

--- Pzl.Did.Mobile.Client/www/didApp/sidemenu/leftSideMenu.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('didApp.leftSidemenuController', [])
-
-.controller('leftSideMenuCtrl',['$scope','$state',leftSideMenuCtrl])
-
-function leftSideMenuCtrl($scope,$state){
-  
-  console.log('leftSideMenuCtrl');
-};
